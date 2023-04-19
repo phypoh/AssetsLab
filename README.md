e14c0c60ea8f834f309c46e3d7854668f4657a47
Phy Poh <phypoh@gmail.com>
2023-04-19 21:40:40 +0800

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
 # AssetsLab
 Python scripts for mass-editing images and animation frames
+
+
+
+## Installation
+
+Python 3 is required. 
+
+
+
+```bash
+pip install -r requirements.txt
+```
+
+
+
+
+
+## Using the Resize Script
+
+This resizes and recenters all images to the largest size. (For animation images)
+
+1. Create an Input and Output folder
+2. Alter the saturation/contrast/brighten degrees and color hex.
+
+
+
+
+
+## Using the Tint Script
+
+Tints, and changes the saturation, contrast and brightness similarly across all images.
+
+1. Create an Input and Output folder
+2. Determine and change the various parameters:
+   - Colour Hex
+   - Saturation degree
+   - Contrast Degree
+   - Brightness Degree
---
+++ tint.py
@@ -0,0 +1,59 @@
+from PIL import Image, ImageOps, ImageEnhance
+from PIL.ImageColor import getcolor, getrgb
+from PIL.ImageOps import grayscale
+import os
+
+color = '#018b86'
+directory = 'Input'
+output_dir = 'Output'
+saturation_degree = 1
+contrast_degree = 1
+brighten_degree = 1.5
+ 
+# iterate over files in
+# that directory
+
+def image_tint(src, tint='#ffffff'):
+    src.load()
+    r, g, b, alpha = src.split()
+    gray = ImageOps.grayscale(src)
+    # gray = ImageOps.autocontrast(gray)
+    result = ImageOps.colorize(gray, (0, 0, 0, 0), tint) 
+    result.putalpha(alpha)
+    return result
+
+
+def saturate(src, saturate=1):
+    converter = ImageEnhance.Color(img)
+    return converter.enhance(saturate)
+
+
+def contrast(src, contrast=1):
+    enhancer = ImageEnhance.Contrast(src)
+    return enhancer.enhance(contrast)
+
+
+def brighten(src, brightness=1):
+    enhancer = ImageEnhance.Brightness(src)
+    return enhancer.enhance(brightness)
+
+def save_img(src, output_path):
+    # src.show()
+    src.save(output_path)
+
+
+for filename in os.listdir(directory):
+    f = os.path.join(directory, filename)
+    # checking if it is a file
+    if os.path.isfile(f):
+        img = Image.open(f)
+        
+        img = image_tint(img, color)
+        img =  saturate(img, saturation_degree)
+        img = contrast(img, contrast_degree)
+        img = brighten(img, brighten_degree)
+        # img =  saturate(img, saturation_degree)
+
+        output_path = os.path.join(output_dir, filename)
+        save_img(img, output_path)
+        # img.show()
